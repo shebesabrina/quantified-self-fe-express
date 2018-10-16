793082b509b5c52dbf2c7222577bedf4e4796b55
Sabrina Robinson <sabrinarob@gmail.com>
2018-10-15 18:37:06 -0600

    Fixes audits and dependencies

--- main.bundle.js
+++ main.bundle.js
@@ -414,9 +414,7 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    // return 'https://qs-go-mine.herokuapp.com/'
-	    // return 'https://qs-express-mine.herokuapp.com/'
-	    return 'https://qs-rails-mine.herokuapp.com';
+	    return 'https://qs-express-mine.herokuapp.com/';
 	  }
 	};
 
